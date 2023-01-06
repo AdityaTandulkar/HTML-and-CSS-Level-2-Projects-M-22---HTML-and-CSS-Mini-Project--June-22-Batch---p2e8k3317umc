7acc7dbe57a15df60a6d5d001f22ba262ea1ab1d
AdityaTandulkar <adityatandulkar20@gmail.com>
2023-01-06 13:03:49 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,1 +1,23 @@
+const accordion = document.querySelector(".accordion");
 
+accordion.addEventListener("click", (event)=>{
+    const radio = event.target.parentNode.firstElementChild.nextElementSibling;
+    // const icon = event.target.parentNode.firstElementChild.firstElementChild.firstElementChild;
+    // let check = false;
+    const removeAttrElem = document.querySelector('[checked]');
+
+    if(removeAttrElem === radio){
+        removeAttrElem.removeAttribute("checked");
+        removeAttrElem.parentNode.firstElementChild.firstElementChild.firstElementChild.style.transform = "rotate(0deg)";
+    }
+    else if(removeAttrElem !== null && removeAttrElem !== radio){
+        removeAttrElem.removeAttribute("checked");
+        removeAttrElem.parentNode.firstElementChild.firstElementChild.firstElementChild.style.transform = "rotate(0deg)";
+        radio.parentNode.firstElementChild.firstElementChild.firstElementChild.style.transform = "rotate(45deg)";
+        radio.setAttribute("checked","");
+    }
+    else{
+        radio.setAttribute("checked","");
+        radio.parentNode.firstElementChild.firstElementChild.firstElementChild.style.transform = "rotate(45deg)";
+    }
+});
